bf4ce4ffee764bea31df0c4ff5af1ca04157c57d
Juan100205 <juanjorianozabaleta@gmail.com>
2026-03-22 01:19:33 -0500

    FullUpdatedBuild

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/RianoDev2.0/',
+  base: '/',
   plugins: [tailwindcss(),react()],
 })
